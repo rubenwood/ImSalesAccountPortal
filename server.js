79a54061ee33c44c59f246fd7c322f2be7a2c2a6
N/A Ruben <ruben.wood1@gmail.com>
2024-02-29 15:24:48 +0000

    Correct a key name

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors());
 
 // AWS METHODS
 AWS.config.update({
-  accessKeyId: process.env.AWS_ACCES_KEY_ID,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_REGION
 });
