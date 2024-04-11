904e9dab87b5cebc313008bfa66c0569e32a76d7
N/A Ruben <ruben.wood1@gmail.com>
2024-04-11 10:40:56 +0100

    Bulk updating tool now uses Id's instead of email addresses

--- public/scripts/bulk-front.js
+++ public/scripts/bulk-front.js
@@ -1,16 +1,20 @@
 import { getSegmentsClicked, getPlayersInSegmentClicked } from "./segments.js";
-import { UpdateUserAcademicAreaByEmail } from "./bulk.js";
+import { UpdatePlayerAcademicArea } from "./bulk.js";
 import { Login } from './PlayFabManager.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('loginButton').addEventListener('click', Login);
     document.getElementById('get-segments-btn').addEventListener('click', () => getSegmentsClicked(document.getElementById('segment-selection')));
     document.getElementById('get-players-btn').addEventListener('click', async () => {
-        let playerEmailList = await getPlayersInSegmentClicked(document.getElementById('segment-selection').value);
-        document.getElementById('player-list').value = playerEmailList;
+        let playerProfiles = await getPlayersInSegmentClicked(document.getElementById('segment-selection').value);
+        let playerIds = playerProfiles.map(player =>{
+            return player.PlayerId;
+        });
+        console.log(playerIds);
+        document.getElementById('player-list').value = playerIds.join('\n');
     });
     document.getElementById('update-area-btn').addEventListener('click', () =>
-        UpdateUserAcademicAreaByEmail(document.getElementById('player-list').value, document.getElementById('desired-area-txt').value)
+        UpdatePlayerAcademicArea(document.getElementById('player-list').value, document.getElementById('desired-area-txt').value)
     );
 });
 window.onload = function() {
