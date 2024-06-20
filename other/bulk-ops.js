067c12f26d02a5677c5296d15affc4197ba1cb10
N/A Ruben <ruben.wood1@gmail.com>
2024-06-20 15:16:39 +0100

    Modified property name for StripeSubscriptionId

--- other/bulk-ops.js
+++ other/bulk-ops.js
@@ -251,7 +251,7 @@ async function convertStripeSubData() {
 
         let newSubData = {
             Platform: "stripe",
-            StripeSubscriptionID: "", // need to get this from stripe
+            StripeSubscriptionId: "", // need to get this from stripe
             Product: userProdInterval,
             PurchaseDate: formatDate(purchaseDate),
             SubStatus: status,
