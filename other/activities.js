4df6237d4f6c510fd3f5c262085a0f1d5f220a2e
N/A Ruben <ruben.wood1@gmail.com>
2024-09-03 08:39:40 +0100

    Refined activity search script

--- other/activities.js
+++ other/activities.js
@@ -4,8 +4,6 @@ const axios = require('axios');
 const activitiesRouter = express.Router();
 const { Pool } = require('pg');
 
-const { getTotalUsageRowCount } = require('../database/database');
-
 const pool = new Pool({
     user: process.env.PGUSER,
     host: process.env.PGHOST,
@@ -16,92 +14,6 @@ const pool = new Pool({
     ssl: {
         rejectUnauthorized: false,
     },
-});
-
-// Modified route that takes in an array of query param get-activity-report-id?activities=activity_id1,activity_id2
-// dentalID_prac,instrumentID_prac
-activitiesRouter.get('/get-activity-report-id', async (req, res) => {
-    console.log("called");
-
-    try {
-        const activityIds = req.query.activities.split(',');
-        const config = { headers: { 'x-secret-key': process.env.SERVER_SEC } };
-        const chunkSize = 10000;
-        let totalErrors = 0;
-        let allPlayersWithActivity = new Map(activityIds.map(id => [id, new Set()]));
-
-        async function processChunk(startRow) {
-            try {
-                let url = `http://${process.env.SERVER_URL}:${process.env.PORT}/db/usagedata`+
-                `?start=${startRow}&end=${startRow + chunkSize - 1}`;
-                const response = await axios.get(url, config);
-                const respDataRows = response.data;
-
-                let errorAmount = 0;
-
-                for (const row of respDataRows) {
-                    try {
-                        console.log(row);
-                        let playerData = row.UsageDataJSON?.Data?.PlayerData ?? undefined;
-                        if (playerData) {
-                            let playerDataToJson = JSON.parse(playerData.Value);
-                            let playerActivities = playerDataToJson.activities;
-                            playerActivities.forEach(activity => {
-                                if (activityIds.includes(activity.activityID)) {
-                                    allPlayersWithActivity.get(activity.activityID).add(row);
-                                }
-                            });
-                        }
-                    } catch (error) {
-                        errorAmount++;
-                        console.error('Error processing player data:', error);
-                    }
-                }
-                return { errorAmount };
-            } catch (error) {
-                console.error('Error processing chunk:', error.message);
-                return { errorAmount: 1 }; // Assuming 1 error if chunk processing fails
-            }
-        }
-
-        // Determine the total number of chunks required
-        const totalRows = await getTotalUsageRowCount();
-        console.log(`total rows: ${totalRows}`);
-        const totalChunks = Math.ceil(totalRows / chunkSize);
-        const chunkPromises = [];
-
-        for (let i = 0; i < totalChunks; i++) {
-            const startRow = i * chunkSize;
-            chunkPromises.push(processChunk(startRow));
-        }
-
-        // Process all chunks concurrently
-        console.log("processing....");
-        const results = await Promise.all(chunkPromises);
-        results.forEach(({ errorAmount }) => {
-            totalErrors += errorAmount;
-        });
-
-        let outputList = [];
-        allPlayersWithActivity.forEach((players, activityId) => {
-            let totalPlays = calcTotalPlaysPerActivity(Array.from(players), activityId);
-            let output = {
-                activityID: activityId,
-                activityName: activityId,// TODO: get activity title
-                uniquePlays: players.size,
-                plays: totalPlays,
-                users: Array.from(players)
-            }
-            outputList.push(output);
-        });
-
-        console.log(`Processed ${totalChunks} chunks (chunk size: ${chunkSize} rows) with ${totalErrors} errors.`);
-
-        res.json(outputList);
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ message: 'Failed to generate report', error: error.message });
-    }
 });
 
 // GET USERS BY ACTIVITY ID
