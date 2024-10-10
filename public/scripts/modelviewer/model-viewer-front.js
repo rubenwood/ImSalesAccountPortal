a2a5bd4765caeb2c9e8ba6ddec5fe96fa9ad2d2e
N/A Ruben <ruben.wood1@gmail.com>
2024-10-10 16:01:25 +0100

    model viewer updates

--- public/scripts/modelviewer/model-viewer-front.js
+++ public/scripts/modelviewer/model-viewer-front.js
@@ -1,16 +1,26 @@
 document.addEventListener('DOMContentLoaded', async() => {
 
     const models = [
-        "https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/MusclesOfTheNeck_S1.glb",
-        "https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/Muscles_of_mastication_I.glb"
+        "https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/Debug/public/MusclesOfTheNeck_S1.glb",
+        "https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/Debug/public/Muscles_of_mastication_I.glb"
     ];
 
     console.log(window.location.search);
     const params = Object.fromEntries(new URLSearchParams(window.location.search)); 
     
     const modelViewer = document.getElementById('model-viewer');
-    modelViewer.src = models[params.model];
+
+    let index = 0;
+    index = params.model;
+    if(params.model >= models.length){ index = models.length; }
+    if(params.model < 0){ index = 0; }
+
+    modelViewer.src = models[index];
 });
 window.onload = function() {
     //document.getElementById('loginModal').style.display = 'block';
 };
+
+async function getPresignedAssetURLs(){
+
+}
