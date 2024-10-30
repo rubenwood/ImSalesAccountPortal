61c4a95f413e80bdaf609391dc9427b7dae89078
N/A Ruben <ruben.wood1@gmail.com>
2024-10-30 08:15:00 +0000

    Removed some old code

--- public/scripts/modelviewer/model-viewer-front.js
+++ public/scripts/modelviewer/model-viewer-front.js
@@ -1,29 +1,9 @@
 document.addEventListener('DOMContentLoaded', async() => {
 
-    const models = [
-        "https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/Debug/public/MusclesOfTheNeck_SH01.glb",
-        "https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/Debug/public/Muscles_of_mastication_I.glb",
-        "https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/Debug/public/Anatomy_CrossSection2_NoLabels.glb",
-
-        "https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/Debug/public/Extracranial.glb",
-        "https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/Debug/public/Trigeminal_Nerve_Functions2_NoLabels.glb",
-        "https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/Debug/public/The_Optic_Nerve2.glb"
-    ];
-
     console.log(window.location.search);
-    const params = Object.fromEntries(new URLSearchParams(window.location.search)); 
-    
+    const params = Object.fromEntries(new URLSearchParams(window.location.search));     
     const modelViewer = document.getElementById('model-viewer');
-
-    //let index = 0;
-    //index = params.model;
-
     let modelName = params.model;
-
-    //if(params.model >= models.length){ index = models.length; }
-    //if(params.model < 0){ index = 0; }
-
-    //modelViewer.src = models[index];
     modelViewer.src = `https://s3.eu-west-1.amazonaws.com/com.immersifyeducation.cms/Models/Debug/public/${modelName}`;
 });
 window.onload = function() {
